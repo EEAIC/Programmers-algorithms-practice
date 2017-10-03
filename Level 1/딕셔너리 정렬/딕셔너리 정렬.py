e18cf5caa35868df4c79227041b1ad1805fb86e3
JK Lim <artherlim@gmail.com>
2017-10-03 23:10:51 +0900

    딕셔너리 정렬

---
+++ Level 1/딕셔너리 정렬/딕셔너리 정렬.py
@@ -0,0 +1,14 @@
+def sort_dictionary(dic):
+    '''입력받은 dic의 각 키와 값을 튜플로 만든 다음, 키 값을 기준으로 정렬해서 리스트에 넣으세요. 그 리스트를 return하면 됩니다.'''
+    rst = []
+    key_list = list(dic.keys())
+    key_list.sort()
+    print(key_list)
+    for i in range(len(key_list)):
+        rst.append((key_list[i],dic.get(key_list[i])))
+
+    return rst
+
+
+# 아래는 테스트로 출력해 보기 위한 코드입니다.
+print(sort_dictionary({"김철수": 78, "이하나": 97, "정진원": 88}))
