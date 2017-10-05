ab709be3319e58f749a76b0c6c12971e6db5e15a
CMD <mac@JeongKee-MacBook-Pro.local>
2017-10-06 05:24:44 +0900

    행렬의 곱셈

--- Level 2/행렬의 곱셈/행렬의 곱셈.py
+++ Level 2/행렬의 곱셈/행렬의 곱셈.py
@@ -3,15 +3,15 @@ def productMatrix(A, B):
     tmp_Sum = 0
     for i in range(len(A)):
         tmp = []
-        for j in range(len(A[0])):
+        for j in range(len(B[0])):
             tmp_Sum = 0
-            for k in range(len(B)):
-                tmp_Sum += A[i][k] * B[k][j]
+            for k in range(len(A[0])):
+            	tmp_Sum += A[i][k] * B[k][j]
             tmp.append(tmp_Sum)
         answer.append(tmp)
     return answer
 
 # 아래는 테스트로 출력해 보기 위한 코드입니다.
-a = [ [ 1, 2 ], [ 2, 3 ]]
-b = [[ 3, 4], [5, 6]]
-print("결과 : {}".format(productMatrix(a,b)))
+a = [ [ 1, 2 ], [ 2, 3 ]];
+b = [[ 3, 4], [5, 6]];
+print("결과 : {}".format(productMatrix(a,b)));
