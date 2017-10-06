8bf38799b1b9ffe5aba5fa95561551dc7730f57c
CMD <mac@JeongKee-MacBook-Pro.local>
2017-10-06 18:43:53 +0900

    최솟값 만들기

---
+++ Level 2/최솟값 만들기/최솟값 만들기.py
@@ -0,0 +1,13 @@
+
+def getMinSum(A,B):
+    answer = 0
+    A.sort()
+    B.sort()
+    B.reverse()
+    for i in range(len(A)):
+        answer += A[i] * B[i]
+    return answer
+
+#아래 코드는 출력을 위한 테스트 코드입니다.
+
+print(getMinSum([1,2],[3,4]))
