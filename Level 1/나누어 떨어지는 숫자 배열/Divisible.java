f6b1cc82d10f9c2138d99961b177c6e8d5d39b14
CMD <mac@JeongKee-MacBook-Pro.local>
2017-10-08 00:53:04 +0900

    나누어 떨어지는 숫자 배열

---
+++ Level 1/나누어 떨어지는 숫자 배열/Divisible.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+class Divisible {
+    public int[] divisible(int[] array, int divisor) {
+        //ret에 array에 포함된 정수중, divisor로 나누어 떨어지는 숫자를 순서대로 넣으세요.
+        int num = 0;
+
+        for (int i  =0; i < array.length; i++){
+            if(array[i] % divisor == 0){
+                num++;
+            }
+        }
+        int[] ret = new int[num];
+        num =0;
+        for (int i  =0; i < array.length; i++){
+            if(array[i] % divisor == 0){
+                ret[num] = array[i];
+                num++;
+            }
+        }
+
+        return ret;
+    }
+    // 아래는 테스트로 출력해 보기 위한 코드입니다.
+    public static void main(String[] args) {
+        Divisible div = new Divisible();
+        int[] array = {5, 9, 7, 10};
+        System.out.println( Arrays.toString( div.divisible(array, 5) ));
+    }
+}
