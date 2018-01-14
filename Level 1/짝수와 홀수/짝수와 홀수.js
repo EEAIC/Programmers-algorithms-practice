5fcf5907765791da8665ee1a9493a17def8fbd0b
JK Lim <artherlim@gmail.com>
2018-01-14 18:55:00 +0900

    짝수와 홀수

---
+++ Level 1/짝수와 홀수/짝수와 홀수.js
@@ -0,0 +1,13 @@
+function evenOrOdd(num) {
+    if (!(num % 2)){
+        return "Even";
+    }
+    else {
+      return "Odd";
+    }
+    // 함수를 완성하세요
+  }
+  
+  // 아래는 테스트로 출력해 보기 위한 코드입니다.
+  console.log("결과 : " + evenOrOdd(2));
+  console.log("결과 : " + evenOrOdd(3));
